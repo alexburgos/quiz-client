b663d6cb4f26a2d2deb462dd89d67e7309e499c1
Alex Burgos <alexander.burgos1@gmail.com>
2019-05-03 20:00:54 -0400

    Updates

---
+++ precache-manifest.a63ffd174e5f4e713fac28878df1dcc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3600e1f97ce1af7d0f4b0fdb31e346a",
+    "url": "/quiz-client/index.html"
+  },
+  {
+    "revision": "5032e36d793b9094689e",
+    "url": "/quiz-client/static/css/main.4a6c3ab8.chunk.css"
+  },
+  {
+    "revision": "275114598dfe6bf12129",
+    "url": "/quiz-client/static/js/2.9f73500c.chunk.js"
+  },
+  {
+    "revision": "5032e36d793b9094689e",
+    "url": "/quiz-client/static/js/main.140b7e1a.chunk.js"
+  },
+  {
+    "revision": "abae039bd1e405515600",
+    "url": "/quiz-client/static/js/runtime~main.87fdd00e.js"
+  }
+]);
